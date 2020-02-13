f2aa9ff99a25e0c15abf046453e4df2ea07f0568
B1max <beunomax@yahoo.com.ar>
2020-02-12 22:01:47 -0300

    modificaciones en casa

--- operaNoConm.py
+++ operaNoConm.py
@@ -10,79 +10,69 @@ lock = threading.Lock()
 #con el lock hago que los threads se sincronisen entre si y no bloqueen el mainthread
 def sumarUno(index):
     global var
-    threading.Semaphore(index)
-    with semaforo.acquire():
-        var+=1#seccion critica
+    # with semaforo: otra forma hace
+    var+=1#seccion critica
+    semaforo.release()
 def sumar(cuanto,index):
     global var
-    threading.Semaphore(index)
-    try:
-        var+=cuanto
-    finally:
-        semaforo.release()
-        # lock.release()
+    var+=cuanto
+    semaforo.release()
 def multiplicaPorDos(index):
     global var
-    threading.Semaphore(index)
-    try:
-        var*=2
-    finally:
-        semaforo.release()
+    var*=2
+    semaforo.release()
 def dividirPorDos(index):
     global var
-    threading.Semaphore(index)
-    try:
-        var /=2
-    finally:
-        semaforo.release()
+    var /=2
+    semaforo.release()
 
 #  3 + 1 / 2
 var = 1
 # thDividir = threading.Thread(target=dividirPorDos)
 # thSumar = threading.Thread(target=sumar, args=[3])
-semaforo = threading.Semaphore(1)
+semaforo = threading.Semaphore(0)
 thDividir = threading.Thread(target=dividirPorDos,args=[0])
 thSumar = threading.Thread(target=sumar, args=[3,1])
-thSumar.start()
 thDividir.start()
-# thDividir.join()
-
+semaforo.acquire()
+thSumar.start()
+thSumar.join()
 logging.info(f'valor final del primero {var}')
 
-#  3 / 2 + 1
-var = 3
-continuar = True
-semaforo = threading.Semaphore(1)
-thDividir = threading.Thread(target=dividirPorDos,args=[0])
-thSumar = threading.Thread(target=sumarUno,args=[1])
-thDividir.start()
-thSumar.start()
-logging.info(f'valor final del segundo {var}')
+# #  3 / 2 + 1
+# var = 3
+# continuar = True
+# semaforo = threading.Semaphore(1)
+# thDividir = threading.Thread(target=dividirPorDos,args=[0])
+# thSumar = threading.Thread(target=sumarUno,args=[1])
+# thDividir.start()
+# thSumar.start()
+# logging.info(f'valor final del segundo {var}')
 
-#  (3 + 1 / 2) / 2
-var = 1
-semaforo = threading.Semaphore(5)
-continuar = True
-thDividir = threading.Thread(target=dividirPorDos,args=[0])
-thSumarA = threading.Thread(target=sumarUno,args=[1])
-thSumarB = threading.Thread(target=sumarUno,args=[2])
-thSumarC = threading.Thread(target=sumarUno,args=[3])
-thDividirB = threading.Thread(target=dividirPorDos,args=[4])
-hilos = [thDividir,thSumarA,thSumarB,thSumarC,thDividirB]
-for hilo in hilos:
-    hilo.start()
-    # hilo.join()
-logging.info(f'valor final del tercero {var}')
+# #  (3 + 1 / 2) / 2
+# var = 1
+# semaforo = threading.Semaphore(5)
+# continuar = True
+# thDividir = threading.Thread(target=dividirPorDos,args=[0])
+# thSumarA = threading.Thread(target=sumarUno,args=[1])
+# thSumarB = threading.Thread(target=sumarUno,args=[2])
+# thSumarC = threading.Thread(target=sumarUno,args=[3])
+# thDividirB = threading.Thread(target=dividirPorDos,args=[4])
+# hilos = [thDividir,thSumarA,thSumarB,thSumarC,thDividirB]
+# for hilo in hilos:
+#     hilo.start()
+#     # hilo.join()
+# logging.info(f'valor final del tercero {var}')
 
-#   (3 / 2 + 1) / 2
-var = 3
-semaforo = threading.Semaphore(2)
-continuar = True
-thDividir = threading.Thread(target=dividirPorDos,args=[0])
-thSumar = threading.Thread(target=sumarUno,args=[1])
-thDividirB = threading.Thread(target=dividirPorDos,args=[2])
-hilos = [thDividir,thSumar,thDividirB]
-for hilo in hilos:
-    hilo.start()
-    hilo.join()
-logging.info(f'valor final del cuarto {var}')
+# #   (3 / 2 + 1) / 2
+# var = 3
+# semaforo = threading.Semaphore(2)
+# continuar = True
+# thDividir = threading.Thread(target=dividirPorDos,args=[0])
+# thSumar = threading.Thread(target=sumarUno,args=[1])
+# thDividirB = threading.Thread(target=dividirPorDos,args=[2])
+# hilos = [thDividir,thSumar,thDividirB]
+# for hilo in hilos:
+#     hilo.start()
+#     hilo.join()
+# logging.info(f'valor final del cuarto {var}')
--- tests.py
+++ tests.py
@@ -1,13 +1,28 @@
 import threading
+import time
 
-sem = threading.Semaphore(4)
-var = 0
-sem.acquire()
-var +=1
-print(f'eje{var}')
-var +=1
-print(f'eje{var}')
-var +=1
-print(f'eje{var}')
-var +=1
-print(f'eje{var}')
+sem2 = threading.Semaphore(1)
+var = -1
+control = 0
+def hilo(id):
+    global var
+    global control
+    if(id%2==0):
+        sem2.acquire()
+    var+=1
+    con = var==id
+    if(con==False):
+        control+=1
+        print(f'id = {id} - var = {var} - con {con}')
+    if(id%2==0):
+        sem2.release()
+
+
+
+# for i in range(10):
+#     threading.Thread(target=hilo, args=[i]).start()
+
+for i in range(1000000):
+    threading.Thread(target=hilo, args=[i]).start()
+time.sleep(1)
+print(f'desincronizaciones -> {control}')
